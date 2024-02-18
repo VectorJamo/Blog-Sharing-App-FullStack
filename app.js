b031184b95716a16dc0cb87066771200f95c07cb
suraj <nsuraj552@gmail.com>
2024-02-18 15:20:25 -0700

    Created login page

--- app.js
+++ app.js
@@ -2,16 +2,17 @@ const express = require('express')
 
 const app = express()
 
+app.listen(3000, () => {
+    console.log('Server is now listening for requests on Port 3000.')
+})
+
 // Set up our view engine
 app.set('view engine', 'ejs')
 
 // Send some static files to the server initially
 app.use(express.static('public'))
 
-app.listen(3000, () => {
-    console.log('Server is now listening for requests on Port 3000.')
-})
-
+// Handle GET requests from client
 app.get('/', (req, res) => {
-    res.render('index', {title: 'Homepage'})
+    res.render('index', {title: 'Login Page'})
 })
